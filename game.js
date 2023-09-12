72c8c66f7bd80151f0eb9d1fe15c913aedfe38f1
efra117 <efra_spartan_117@hotmail.com>
2023-09-12 00:21:09 -0600

    change the for cycle by forEach to make the code more clear

--- game.js
+++ game.js
@@ -19,11 +19,21 @@ function startGame() {
     const mapRowCols = mapRows.map(row => row.trim().split(''));
     console.log({map, mapRows, mapRowCols});
 
-    for(let row=1; row<=10; row++){
-        for(let col=1; col<=10 ; col++) {
-            game.fillText(emojis[mapRowCols[row-1][col-1]],elementSize*col, elementSize*row)
-        }
-    }
+    mapRowCols.forEach((row, rowI) => {
+        row.forEach((col, colI) => {
+        const emoji = emojis[col];
+        const posX= elementSize *(colI+1)
+        const posY= elementSize *(rowI+1)
+        game.fillText(emoji, posX, posY);
+        })
+    });
+
+
+    // for(let row=1; row<=10; row++){
+    //     for(let col=1; col<=10 ; col++) {
+    //         game.fillText(emojis[mapRowCols[row-1][col-1]],elementSize*col, elementSize*row)
+    //     }
+    // }
     
 
     // game.fillRect(0,0,100,100); //(where it starts in X, where it starts in Y, width, heigh)
