45fe5522322a955c1aa076f2983eff4e6075e602
efra117 <efra_spartan_117@hotmail.com>
2023-09-11 19:45:18 -0600

    update game.js to get every element of each map in maps.js in order to show them in the canvas label

--- game.js
+++ game.js
@@ -1,30 +1,30 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d'); //Context created so qe can use the Canvas
 
-window.addEventListener('load', startGame); //We put this Even listener in order to start the js code after the html has been loaded completely
-window.addEventListener('resize', startGame);
+window.addEventListener('load', setCanvasSize); //We put this Even listener in order to start the js code after the html has been loaded completely
+window.addEventListener('resize', setCanvasSize);
+
+let canvasSize;
+let elementSize;
 
 function startGame() {  
 
-    let canvasSize;
-
-    if(window.innerHeight > window.innerWidth) {
-        canvasSize = window.innerWidth*0.75;
-    } else {
-        canvasSize = window.innerHeight*0.75;
-    }
-    canvas.setAttribute('width', canvasSize); // window.innerWidth makes the element responsive
-    canvas.setAttribute('height', canvasSize);
-    
-    const elementSize = canvasSize/10;
-
     console.log({canvasSize, elementSize})
 
-    game.font = `${elementSize}px verdana`;
-    game.fillText(emojis['X'],100, 100)
-    
-   
+    game.font = `${elementSize}px Verdana`;
+    game.textAlign = 'end';
 
+    const map = maps[1];
+    const mapRows = map.trim().split('\n');
+    const mapRowCols = mapRows.map(row => row.trim().split(''));
+    console.log({map, mapRows, mapRowCols});
+
+    for(let row=1; row<=10; row++){
+        for(let col=1; col<=10 ; col++) {
+            game.fillText(emojis[mapRowCols[row-1][col-1]],elementSize*col, elementSize*row)
+        }
+    }
+    
 
     // game.fillRect(0,0,100,100); //(where it starts in X, where it starts in Y, width, heigh)
     // game.clearRect(0,0,50,50); //this function help us to erase a section
@@ -34,5 +34,19 @@ function startGame() {
     // game.textAlign = 'end' //It helps us to move the test in fillText acording to the position defined in X & Y
     // game.fillText('momos',50,50)
 
+}
 
+function setCanvasSize () {
+
+    if(window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth*0.75;
+    } else {
+        canvasSize = window.innerHeight*0.75;
+    }
+    canvas.setAttribute('width', canvasSize); // window.innerWidth makes the element responsive
+    canvas.setAttribute('height', canvasSize);
+    
+    elementSize = canvasSize/10;
+
+    startGame();
 }
